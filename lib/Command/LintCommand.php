b45b4221c647ebea609fa82875fd9ecaba389fb5
Markus Staab <markus.staab@redaxo.de>
2019-04-03 20:12:15 +0200

    Update LintCommand.php

--- lib/Command/LintCommand.php
+++ lib/Command/LintCommand.php
@@ -22,15 +22,15 @@ final class LintCommand extends Command
         $dir = $input->getArgument('dir');
 
         $processes = [];
-        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.yml', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/yaml-lint', '{} +']);
+        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.yml', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/yaml-lint', '{}', '+']);
         $processes[] = $this->asyncProc(['vendor/bin/parallel-lint', '--exclude',  'vendor', $dir]);
-        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.json', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/jsonlint', '{} +']);
+        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.json', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/jsonlint', '{}', '+']);
 
         $this->syncProc(['npm', 'install', 'csslint']);
 
         // we only want to find errors, no style checks
         $csRules = 'order-alphabetical,important,ids,font-sizes,floats';
-        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.css', '!', '-path', '*/vendor/*', '-exec', 'node_modules/.bin/csslint', '--ignore='.$csRules, '{} +']);
+        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.css', '!', '-path', '*/vendor/*', '-exec', 'node_modules/.bin/csslint', /*'--ignore='.$csRules,*/ '{}', '+']);
 
         foreach ($processes as $process) {
             $process->wait();
