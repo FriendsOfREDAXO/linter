f7e5162c26ecab9a3ea32334ddd732eb00408439
Markus Staab <maggus.staab@googlemail.com>
2019-04-03 20:44:02 +0200

    Add nodejs, npm and css-lints (#18)
    css linter implementiert
    
    status code fixes, wenn subcommands abbrechen
    
    cli ausgaben aufgehübscht

--- lib/Command/LintCommand.php
+++ lib/Command/LintCommand.php
@@ -4,7 +4,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process;
 
 final class LintCommand extends Command
@@ -18,28 +18,56 @@ final class LintCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $style = new SymfonyStyle($input, $output);
+        // the "+" on "find ... -exec" makes the find command fail, when the -exec'ed command fails.
         $dir = $input->getArgument('dir');
 
+        /**
+         * @var Process[]
+         */
         $processes = [];
-        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.yml', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/yaml-lint', '{}', ';']);
-        $processes[] = $this->asyncProc(['vendor/bin/parallel-lint', '--exclude',  'vendor', $dir]);
-        $processes[] = $this->asyncProc(['find', $dir, '-name', '*.json', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/jsonlint', '{}', ';']);
+        $processes['YAML checks'] = $this->asyncProc(['find', $dir, '-name', '*.yml', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/yaml-lint', '{}', '+']);
+        $processes['PHP checks'] = $this->asyncProc(['vendor/bin/parallel-lint', '--exclude',  'vendor', $dir]);
+        $processes['JSON checks'] = $this->asyncProc(['find', $dir, '-name', '*.json', '!', '-path', '*/vendor/*', '-exec', 'vendor/bin/jsonlint', '{}', '+']);
 
-        foreach ($processes as $process) {
+        $this->syncProc(['npm', 'install', 'csslint']);
+
+        // we only want to find errors, no style checks
+        $csRules = 'order-alphabetical,important,ids,font-sizes,floats';
+        $processes['CSS checks'] = $this->asyncProc(['find', $dir, '-name', '*.css', '!', '-path', '*/vendor/*', '-exec', 'node_modules/.bin/csslint', '--ignore='.$csRules, '{}', '+']);
+
+        $exit = 0;
+        foreach ($processes as $label => $process) {
             $process->wait();
 
             if (!$process->isSuccessful()) {
-                throw new ProcessFailedException($process);
+                $style->section($label);
+                echo $process->getOutput();
+                echo $process->getErrorOutput();
+                $style->error("$label failed\n");
+                $exit = 1;
+            } else {
+                if ($output->isVerbose()) {
+                    echo $process->getOutput();
+                }
+                $style->success("$label successfull\n");
             }
-
-            echo $process->getOutput();
         }
+
+        return $exit;
     }
 
-    private function asyncProc(array $cmd)
+    private function asyncProc(array $cmd): Process
     {
         $process = new Process($cmd);
         $process->start();
         return $process;
+    }
+
+    private function syncProc(array $cmd)
+    {
+        $syncP = new Process($cmd);
+        $syncP->mustRun();
+        echo $syncP->getOutput();
     }
 }
