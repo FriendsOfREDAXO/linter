449d7d544f9d70fd5451baf2cd5b204b1bbf7a99
Markus Staab <markus.staab@redaxo.de>
2019-04-12 15:53:12 +0200

    make it work when called from a different directory

--- bin/lint-file.sh
+++ bin/lint-file.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-cat "$1" | ../vendor/bin/lint-query
+# give you the full directory name of the script no matter where it is being called from
+# stolen from https://stackoverflow.com/a/246128
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "$1"
+cat "$1" | "$DIR/../vendor/bin/lint-query"
